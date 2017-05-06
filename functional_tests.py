69d210881915361680dac05b3a73fa6457a4789b
Yao Lu <yaolu@tibco-support.com>
2017-05-06 18:01:10 +0800

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# 伊迪丝听说有一个很酷的在线代办事项应用
 		# 她去看了这个应用的首页
@@ -34,20 +39,18 @@ class NewVisitorTest(unittest.TestCase):
 		# 她按回车键后，页面更新了
 		# 待办事项表格中显示了“1：Buy peacock feathers”
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# 页面中又显示了一个文本框，可以输入其他的待办事项
 		# 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
 		# 伊迪丝做事很有条理
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# 页面再次更新，她的清单中显示了这两个代办事项
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# 伊迪丝想知道这个网站是否会记住她的清单
 
